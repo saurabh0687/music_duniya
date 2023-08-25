052a329b9c8fa3b010ff20b049d81b9baee7628e
saurabh <baghelsaurabh687@gmail.com>
2023-08-26 03:20:05 +0530

    using getx

--- lib/controller/Controller.dart
+++ lib/controller/Controller.dart
@@ -1,10 +1,14 @@
 import 'package:get/get.dart';
 
 class ExampleController extends GetxController{
-RxBool notifications=false.obs;
-SetNotifications(bool value){
-  notifications.value=value;
-
+ RxList<String> fruitsList=['Apple','Mango','Banana','Papaya'].obs;
+  RxList<dynamic> fav=[].obs;
+addFav(String value){
+ fav.add(value);
+}
+removeFav(String value)
+{
+  fav.remove(value);
 }
 
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,24 +12,34 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   ExampleController _exampleController= Get.put(ExampleController());
-
+ 
    
   @override
   Widget build(BuildContext context) {
+    print("build");
     return Scaffold(
     
-    
-      body:Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-        Text("Notifications"),
-       Obx(() => Switch(value: _exampleController.notifications.value, onChanged:(value) {
-           _exampleController.SetNotifications(value);
-           
-          },),
-        )
-
-      ],)
+     
+      body:ListView.builder(
+        itemCount: _exampleController.fruitsList.length,
+        itemBuilder: (context, index) {
+        return Card(
+          child: ListTile(
+            onTap: () {
+              if(_exampleController.fav.contains(_exampleController.fruitsList[index].toString())){
+                 _exampleController.removeFav(_exampleController.fruitsList[index].toString());
+              }
+              else{
+                 _exampleController.addFav(_exampleController.fruitsList[index].toString());
+              }
+              },
+            
+            
+            title: Text(_exampleController.fruitsList[index].toString()),
+            trailing: Obx(() =>  Icon(Icons.favorite ,color: _exampleController.fav.contains(_exampleController.fruitsList[index].toString())?Colors.red:Colors.white,)),
+          ),
+        );
+      })
     );
   }
 }
